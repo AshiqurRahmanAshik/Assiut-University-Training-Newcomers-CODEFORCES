1cc63beeb49167c3f1f0a0ea039706f3ba1ff76d
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-10-17 12:51:39 +0600

    C_Replacement

---
+++ Sheet #3 (Arrays)/B_Searching.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+  scanf("%d", &n);
+  int arr[n];
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
+  }
+  int x;
+  int found = 0;
+  scanf("%d", &x);
+  for (int i = 0; i < n; i++) {
+    if (arr[i] == x) {
+      printf("%d", i);
+      found = 1;
+      break;
+    }
+  }
+  if (!found) {
+    printf("-1");
+  }
+
+  return 0;
+}
