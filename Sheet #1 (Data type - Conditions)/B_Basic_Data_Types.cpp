68f88f903f2fa25c59fc5fbd4c47b8922b16f4ab
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2026-01-01 01:25:41 +0600

    Basic Data Types

---
+++ Sheet #1 (Data type - Conditions)/B_Basic_Data_Types.cpp
@@ -0,0 +1,23 @@
+/*
+Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/B
+ Date:  01/01/26
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    long long int b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b >> c >> d >> e;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+    return 0;
+}
