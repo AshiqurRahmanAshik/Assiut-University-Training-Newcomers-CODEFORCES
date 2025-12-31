5c9272b7396b1debd019275eb5708953d12e8955
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2026-01-01 01:28:50 +0600

    Simple Calculator

---
+++ Sheet #1 (Data type - Conditions)/C_Simple_Calculator.cpp
@@ -0,0 +1,17 @@
+/*
+Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/C
+ Date:  01/01/26
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int x, y;
+    cin >> x >> y;
+    cout << x << " + " << y << " = " << x + y << endl;
+    cout << x << " * " << y << " = " << x * y << endl;
+    cout << x << " - " << y << " = " << x - y << endl;
+    return 0;
+}
