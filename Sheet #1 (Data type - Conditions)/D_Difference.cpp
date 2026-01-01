1dcadde59e2250bb8bd3b3c0556525db10669814
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2026-01-02 00:48:24 +0600

    Differnce

---
+++ Sheet #1 (Data type - Conditions)/D_Difference.cpp
@@ -0,0 +1,15 @@
+/*
+Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/D
+ Date:  02/01/26
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << "Difference = " << (a * b) - (c * d);
+    return 0;
+}
