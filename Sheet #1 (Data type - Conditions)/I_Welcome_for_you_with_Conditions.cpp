d02628d9961599657fcb60d99d63200a624de858
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2026-01-03 00:56:09 +0600

    Welcome for you with conditions

---
+++ Sheet #1 (Data type - Conditions)/I_Welcome_for_you_with_Conditions.cpp
@@ -0,0 +1,18 @@
+/*
+Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/I
+ Date:  03/01/26
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a >= b)
+        cout << "Yes";
+    else
+        cout << "No";
+    return 0;
+}
