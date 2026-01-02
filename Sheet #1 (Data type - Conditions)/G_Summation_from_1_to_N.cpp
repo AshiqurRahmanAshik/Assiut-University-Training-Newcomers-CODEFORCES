ef1431606808a0412c79be18103a3fb106ff4c73
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2026-01-03 00:48:24 +0600

    Summation from 1 to N

---
+++ Sheet #1 (Data type - Conditions)/G_Summation_from_1_to_N.cpp
@@ -0,0 +1,15 @@
+/*
+Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/G
+ Date:  03/01/26
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int n;
+    cin >> n;
+    cout << n * (n + 1) / 2 << endl;
+    return 0;
+}
