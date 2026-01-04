ef1db04ae242f27feeb1874b7871e1dc8ff47aaf
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2026-01-04 11:05:56 +0600

    The Brothers

---
+++ Sheet #1 (Data type - Conditions)/L_The_Brothers.cpp
@@ -0,0 +1,23 @@
+/*
+Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/L
+ Date:  04/01/26
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string f1, s1, f2, s2;
+    cin >> f1 >> s1 >> f2 >> s2;
+    if (s1 == s2)
+    {
+        cout << "ARE Brothers" << endl;
+    }
+    else
+    {
+        cout << "NOT" << endl;
+    }
+
+    return 0;
+}
